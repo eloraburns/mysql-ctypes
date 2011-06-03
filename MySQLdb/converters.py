62030cc55a61f7a5711072dde8fd8ec72064be1d
Alex Gaynor <alex.gaynor@gmail.com>
2011-06-02 18:43:48 -0700

    Revert "integirty error type, and float/double decoding"
    This reverts commit f68faf571a399a5e4cec33c0ec9b567759b87ac8.

--- MySQLdb/converters.py
+++ MySQLdb/converters.py
@@ -71,9 +71,6 @@ _simple_field_decoders = {
     field_types.LONG: int,
     field_types.LONGLONG: int,
 
-    field_types.FLOAT: float,
-    field_types.DOUBLE: float,
-
     field_types.NEWDECIMAL: Decimal,
 
     field_types.DATETIME: datetime_decoder,
