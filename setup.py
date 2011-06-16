36c965d3cb39175dd9d172417925e9e4bbb5681b
Alex Gaynor <alex.gaynor@gmail.com>
2011-06-16 11:42:03 -0700

    Don't install tests/ dir.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="mysql-ctypes",
     description=("A MySQL wrapper that uses ctypes, aims to be a drop-in "
         "replacement for MySQLdb"),
-    packages=find_packages(),
+    packages=["MySQLdb", "MySQLdb.constants"],
 )
