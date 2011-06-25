2871b6b421b44318f6f2adcf090891bcf70bc64e
Alex Gaynor <alex.gaynor@gmail.com>
2011-06-24 17:28:40 -0700

    Added LICENSE, updated README and setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from setuptools import setup
 
 setup(
     name="mysql-ctypes",
+    version = "0.5",
+    author = "Alex Gaynor and Quora",
+    author_email = "alex.gaynor@gmail.com",
     description=("A MySQL wrapper that uses ctypes, aims to be a drop-in "
         "replacement for MySQLdb"),
     packages=["MySQLdb", "MySQLdb.constants"],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database",
+    ],
 )
